ed4551968f2bc66859c981cbea8187639111573e
abhiyadav1711 <abhinavyadav1711@gmail.com>
2026-09-04 14:05:08 +0530

    Update urls.py

--- hospital/urls.py
+++ hospital/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
+
+    path('patients/', views.patients_view, name='patients'),
+    path('patients/<int:pk>/delete/', views.patient_delete, name='patient_delete'),
 ]
