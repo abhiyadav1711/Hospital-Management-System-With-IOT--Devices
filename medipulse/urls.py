8c72edd71d018702286730529b2b2690fd99858c
abhiyadav1711 <abhinavyadav1711@gmail.com>
2026-09-04 14:20:27 +0530

    Update urls.py

--- medipulse/urls.py
+++ medipulse/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('hospital.api_urls')),
     path('', include('hospital.urls')),
 ]
